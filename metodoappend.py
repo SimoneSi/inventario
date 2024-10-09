17b95a6ba2395fbf3bd11c60128fefdf94fec108
Simone Lima <simone_lima5@yahoo.com.br>
2024-10-09 15:57:36 -0300

    Arquivo foi simplificado armazenando em uma lista de nome equipamento os equipamentos do inventario assim, de igual modo, armazenamos em uma outra lista de nome inventario a lista equipamento, logo cada lista equipamento imputada ocupara um indice na lista inventario

--- metodoappend.py
+++ metodoappend.py
@@ -1,47 +1,39 @@
-equipamentos = []
-valores = []
-seriais = []
-departamentos = []
+inventario=[]
 resposta="S"
 while resposta=="S":
- equipamentos.append(input("Equipamento: "))
- valores.append(float(input("Valor: ")))
- seriais.append(int(input("Número Serial: ")))
- departamentos.append(input("Departamento: "))
- resposta=input("Digite \"S\" para continuar: ").upper()
+  equipamento= [input("Equipamento: "),
+   float(input("Valores: ")), 
+   int(input("Seriais: ")),
+   input("Departamentos: ")]
+  inventario.append(equipamento)
+  resposta=input("Digite \"S\" para continuar: ").upper()
  
-for indice in range(0,len(equipamentos)):
- print("\nEquipamento..: ", (indice+1))
- print("Nome.........: ", equipamentos[indice])
- print("Valor........: ", valores[indice])
- print("Serial.......: ", seriais[indice])
- print("Departamento.: ", departamentos[indice])
+for elemento in inventario:
+ print("Nome.........: ", elemento[0])
+ print("Valor........: ", elemento[1])
+ print("Serial.......: ", elemento[2])
+ print("Departamento.: ", elemento[3])
 
 busca=input("\nDigite o nome do equipamento que deseja buscar: ")
-for indice in range(0,len(equipamentos)):
- if busca==equipamentos[indice]:
-    print("Valor..: ", valores[indice])
-    print("Serial.:", seriais[indice])
+for elemento in inventario:
+ if busca==elemento[0]:
+    print("Valor..: ", elemento[1])
+    print("Serial.:", elemento[2])
 
 depreciacao=input("\nDigite o nome do equipamento que será depreciado: ")
-for indice in range(0,len(equipamentos)):
- if depreciacao==equipamentos[indice]:
-    print("Valor antigo: ", valores[indice])
-    valores[indice] = valores[indice] * 0.9
-    print("Novo valor: %.2f " % valores[indice])
+for elemento in inventario:
+ if depreciacao==elemento[0]:
+    print("Valor antigo: ", elemento[1])
+    elemento[1] = elemento[1] * 0.9
+    print("Novo valor: %.2f " % elemento[1])
 
 serial=int(input("\nDigite o serial do equipamento que será excluido: "))
-for indice in range(0, len(departamentos)):
-   if seriais[indice]==serial:
-      del departamentos[indice]
-      del equipamentos[indice]
-      del seriais[indice]
-      del valores[indice]
-      break
+for elemento in inventario:
+   if elemento[2]==serial:
+      inventario.remove(elemento)
 
-for indice in range(0,len(equipamentos)):
-   print("\nEquipamento..: ", (indice+1))
-   print("Nome.........: ", equipamentos[indice])
-   print("Valor........:  %.2f " % valores[indice])
-   print("Serial.......: ", seriais[indice])
-   print("Departamento.: ", departamentos[indice]) 
+for elemento in inventario:
+ print("Nome.........: ", elemento[0])
+ print("Valor........: ", elemento[1])
+ print("Serial.......: ", elemento[2])
+ print("Departamento.: ", elemento[3])
